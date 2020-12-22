217778c9868a5716173722f48af6d5a372550c60
niversdack <bb6bbe@gmail.com>
2020-12-22 13:03:46 +0500

    Gitflowtest

--- src/todos-micro/main.go
+++ src/todos-micro/main.go
@@ -50,7 +50,8 @@ func AuthMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		token := c.Request.Header["Authorization"][0]
 		claims := utils.GetClaims("napoleonJwT24", token)
-
+		var resadas int
+		_ = resadas
 		var user models.User
 		database.DB.Where("username=?", claims.Username).First(&user)
 		if user.ID != claims.ID {
